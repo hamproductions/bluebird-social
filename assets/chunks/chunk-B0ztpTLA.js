8316c9931d1f65bbc09a940c1acbc0dafada5983
HamBot CI <ci@ham-san.net>
2025-08-08 14:48:03 +0000

    Updates

---
+++ assets/chunks/chunk-B0ztpTLA.js
@@ -0,0 +1,2 @@
+import{b as a,aJ as t}from"./chunk-QsWXeuEA.js";const s=a("h2",t,{defaultProps:{variant:"heading"}});export{s as H};
+//# sourceMappingURL=chunk-B0ztpTLA.js.map
--- assets/chunks/chunk-C-_C22PA.js
+++
@@ -1,2 +0,0 @@
-import{b as a,aI as t}from"./chunk-BftUmRwN.js";const s=a("h2",t,{defaultProps:{variant:"heading"}});export{s as H};
-//# sourceMappingURL=chunk-C-_C22PA.js.map
