938fe9c068511fd94d8e98a78551526ee4666e4e
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2025-08-10 05:22:09 +0900

    Updates

--- assets/chunks/chunk-B0ztpTLA.js
+++
@@ -1,2 +0,0 @@
-import{b as a,aJ as t}from"./chunk-QsWXeuEA.js";const s=a("h2",t,{defaultProps:{variant:"heading"}});export{s as H};
-//# sourceMappingURL=chunk-B0ztpTLA.js.map
---
+++ assets/chunks/chunk-ChnWyLbn.js
@@ -0,0 +1,2 @@
+import{d as a,aK as t}from"./chunk-qOGw5svb.js";const s=a("h2",t,{defaultProps:{variant:"heading"}});export{s as H};
+//# sourceMappingURL=chunk-ChnWyLbn.js.map
