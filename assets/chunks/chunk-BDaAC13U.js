0a5cf1fc30b685e132ecca772b47eefe73176ae2
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2025-09-11 14:52:01 +0900

    Updates

--- assets/chunks/chunk-BDaAC13U.js
+++
@@ -1,2 +0,0 @@
-import{p as o}from"./chunk-KIQZP-MN.js";const t=r=>o.join("/bluebird-social",r);export{t as g};
-//# sourceMappingURL=chunk-BDaAC13U.js.map
---
+++ assets/chunks/chunk-Dp3fnOKI.js
@@ -0,0 +1,2 @@
+import{E as a,b5 as t}from"./chunk-DcC3dirS.js";const s=a("h2",t,{defaultProps:{variant:"heading"}});export{s as H};
+//# sourceMappingURL=chunk-Dp3fnOKI.js.map
