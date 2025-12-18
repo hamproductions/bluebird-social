1cbdfe32ffc5f6cd63d5b1a96403e857662874b5
HamBot CI <ci@ham-san.net>
2025-12-18 08:31:25 +0000

    Updates

--- assets/chunks/chunk-Bee0XqH4.js
+++
@@ -1,1 +0,0 @@
-import{J as a}from"./chunk-B3nAzSY-.js";function o(){return a()?.data}export{o as u};
--- assets/chunks/chunk-Dgwd5IeA.js
+++
@@ -1,1 +0,0 @@
-import{d as a,aH as t}from"./chunk-B3nAzSY-.js";const s=a("h2",t,{defaultProps:{variant:"heading"}});export{s as H};
